f7f53e848c4ac490ea8bd11c09ce0479c70bab8a
brainytech <140586866+VanshJani@users.noreply.github.com>
2026-07-21 18:01:21 +0530

    adding next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,14 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Generates a fully static site (HTML + CSS + JS)
-//   // No Node.js server needed — host on Vercel, Netlify, Cloudflare Pages, etc.
-//   output: "export",
-
-//   // Required for static export — Next.js image optimisation needs a server,
-//   // so we disable it and let the browser handle images natively.
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Generates a fully static site (HTML + CSS + JS)
+  // No Node.js server needed — host on Vercel, Netlify, Cloudflare Pages, etc.
+  output: "export",
+
+  // Required for static export — Next.js image optimisation needs a server,
+  // so we disable it and let the browser handle images natively.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
